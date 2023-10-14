a649778db4631f7b3aa00862af74e15a342f7fcd
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-10-13 23:55:25 -0400

    Create cheatsheet.r

---
+++ cheatsheet.r
@@ -0,0 +1,24 @@
+# Print statement
+print("Hola Mundo!") 
+# Output: "Hola Mundo!"
+
+# Equal operator assignment.
+primes = c(2,3,5,7)      
+
+# Leftward operator assignment.
+strings <- c("learn","R")   
+
+# Rightward operator assignment.   
+c(TRUE,FALSE) -> bools       
+
+print(primes)
+# Output: 2 3 5 7
+
+cat (primes, "are prime numbers", "\n")
+# Output: 2 3 5 7 are prime numbers 
+
+cat (strings , "is from a concatenated string","\n")
+# Output: learn R is from a concatenated string 
+
+cat (bools, "are booleans","\n")
+# Output: TRUE FALSE are booleans 
